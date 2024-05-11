5dd0dd5b007ee95a69b696e2b21a20dabef33b7b
Maaz Nonsola <maaznonsola786@gmail.com>
2024-05-11 13:18:49 +0530

    Added SearchIcon import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
