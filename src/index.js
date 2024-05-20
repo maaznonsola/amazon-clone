51abafca7a4c709091cf34fdcd9e5bcb7bc1ba71
Maaz Nonsola <maaznonsola786@gmail.com>
2024-05-20 11:57:35 +0530

    Basket Functionality with React Context API

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import reducer, {initialState} from "./context-api/reducer";
+import {StateProvider} from "./context-api/StateProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
