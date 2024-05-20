a57f82c0ccc998eeab45d0a86d67938fd06f77bd
Maaz Nonsola <maaznonsola786@gmail.com>
2024-05-20 14:25:15 +0530

    Configured Firebase and added Login Authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import {auth} from "./firebase/firebase";
+import {useStateValue} from "./context-api/StateProvider";
+
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
@@ -17,6 +45,7 @@ function App() {
               </>
             }
           />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,16 @@ import SearchIcon from "@mui/icons-material/Search";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import {Link} from "react-router-dom";
 import {useStateValue} from "../context-api/StateProvider";
+import {auth} from "../firebase/firebase";
 
 function Header() {
   const [{basket, user}, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
 
   return (
     <div className="header">
@@ -24,10 +31,16 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        <div className="header__option">
-          <span className="header__optionLineOne">Hello Guest</span>
-          <span className="header__optionLineTwo">Sign In</span>
-        </div>
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">
+              Hello {!user ? "Guest" : user.email}
+            </span>
+            <span className="header__optionLineTwo">
+              {user ? "Sign Out" : "Sign In"}
+            </span>
+          </div>
+        </Link>
 
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
