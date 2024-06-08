e374de0fa20937ae1965afed207b93d0a2edc6ed
Maaz Nonsola <maaznonsola786@gmail.com>
2024-06-08 13:18:59 +0530

    Cleaning product fetching code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,88 +1,73 @@
 import React from "react";
 import "./Home.css";
 import Product from "./Product";
+import {
+  products_R1,
+  products_R2,
+  products_R3,
+  products_R4,
+  bannerImg,
+} from "../products/products";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <img
-          className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-          alt=""
-        />
+        <img className="home__image" src={bannerImg} alt="" />
         <div className="home__row">
-          <Product
-            id="123456"
-            title="Redmi Note 9 Pro (Interstellar Black, 4GB RAM, 64GB Storage)"
-            price={13999}
-            image="https://images-eu.ssl-images-amazon.com/images/I/51Fc-xm6bKL._AC_US160_.jpg"
-            rating={4}
-          />
-          <Product
-            id="123457"
-            title="Redmi 9 (Sky Blue, 4GB RAM, 64GB Storage)"
-            price={8999}
-            image="https://images-eu.ssl-images-amazon.com/images/I/41Ki5S4HEoL._AC_US160_.jpg"
-            rating={5}
-          />
+          {products_R1.map((item) => {
+            return (
+              <Product
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
         </div>
 
         <div className="home__row">
-          <Product
-            id="123458"
-            title="Samsung Galaxy M31s(6GB RAM, 128GB Storage)"
-            price={19499}
-            image="https://images-eu.ssl-images-amazon.com/images/I/41cVLRT2gyL._AC_US160_.jpg"
-            rating={5}
-          />
-          <Product
-            id="123459"
-            title="Nokia 105 2019 (Single SIM, Black)"
-            price={1499}
-            image="https://images-eu.ssl-images-amazon.com/images/I/31Z2ee9oYQL._AC_US160_.jpg"
-            rating={4}
-          />
-          <Product
-            id="123460"
-            title="boAt Sports Wireless Headset"
-            price={1999}
-            image="https://images-eu.ssl-images-amazon.com/images/I/41j7VEAjdRL._AC_US160_.jpg"
-            rating={5}
-          />
+          {products_R2.map((item) => {
+            return (
+              <Product
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
         </div>
 
         <div className="home__row">
-          <Product
-            id="123461"
-            title="pTron Bassbuds in-Ear True Wireless Bluetooth Headphones(Black)"
-            price={1099}
-            image="https://images-eu.ssl-images-amazon.com/images/I/418XVTLmVNL._AC_US160_.jpg"
-            rating={3}
-          />
-          <Product
-            id="90829332"
-            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
-            price={1094.98}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
-          />
+          {products_R3.map((item) => {
+            return (
+              <Product
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
         </div>
+
         <div className="home__row">
-          <Product
-            id="12321341"
-            title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
-            price={11.96}
-            rating={5}
-            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-          />
-          <Product
-            id="49538094"
-            title="Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl"
-            price={239.0}
-            rating={4}
-            image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
-          />
+          {products_R4.map((item) => {
+            return (
+              <Product
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            );
+          })}
         </div>
 
         <div className="home__row">
---
+++ src/products/products.js
@@ -0,0 +1,121 @@
+export const products_R1 = [
+  {
+    id: 1,
+    title: "Oculus Quest All-in-one VR Gaming Headset – 64GB",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/31pEe2taIPL._AC_US327_FMwebp_QL65_.jpg",
+    price: 11.96,
+    rating: 4,
+  },
+  {
+    id: 2,
+    title: "Nintendo Switch with Neon Blue and Neon Red Joy‑Con - HAC-001(-01)",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41DQoLIfsRL._AC_US327_FMwebp_QL65_.jpg",
+    price: 15.96,
+    rating: 3,
+  },
+  {
+    id: 3,
+    title: "Xbox game",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2B5ECC8E3DA42415._V531815325_.jpg",
+    price: 23.96,
+    rating: 5,
+  },
+  {
+    id: 4,
+    title: "The LeanStartup2: How constant innovative creators",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg",
+    price: 9.96,
+    rating: 4,
+  },
+  {
+    id: 5,
+    title: "Play station game pad",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/G/01/amazonglobal/images/email/asins/DURM-2B638E86650FFF18._V531815327_.jpg",
+    price: 19.96,
+    rating: 5,
+  },
+];
+
+export const products_R2 = [
+  {
+    id: 1,
+    title: "Redmi Note 9 Pro (Interstellar Black, 4GB RAM, 64GB Storage)",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/51Fc-xm6bKL._AC_US160_.jpg",
+    price: 13999,
+    rating: 4,
+  },
+  {
+    id: 2,
+    title: "Redmi 9 (Sky Blue, 4GB RAM, 64GB Storage)",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/41Ki5S4HEoL._AC_US160_.jpg",
+    price: 8999,
+    rating: 3,
+  },
+  {
+    id: 3,
+    title: "Samsung Galaxy M31s(6GB RAM, 128GB Storage)",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/41cVLRT2gyL._AC_US160_.jpg",
+    price: 19499,
+    rating: 4,
+  },
+  {
+    id: 4,
+    title: "Nokia 105 2019 (Single SIM, Black)",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/31Z2ee9oYQL._AC_US160_.jpg",
+    price: 1499,
+    rating: 5,
+  },
+];
+
+export const products_R3 = [
+  {
+    id: 1,
+    title: "pTron Bassbuds in-Ear True Wireless Bluetooth Headphones(Black)",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/418XVTLmVNL._AC_US160_.jpg",
+    price: 1099,
+    rating: 3,
+  },
+  {
+    id: 2,
+    title:
+      "Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg",
+    price: 1094.98,
+    rating: 4,
+  },
+];
+
+export const products_R4 = [
+  {
+    id: 1,
+    title:
+      "The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg",
+    price: 11.96,
+    rating: 4,
+  },
+  {
+    id: 2,
+    title:
+      "Kenwood kMix Stand Mixer for Baking, Stylish Kitchen Mixer with K-beater, Dough Hook and Whisk, 5 Litre Glass Bowl",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg",
+    price: 8999,
+    rating: 5,
+  },
+];
+
+export const bannerImg =
+  "https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg";
