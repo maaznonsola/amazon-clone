ac327a029e3d57f84bf54f0ff1d66b3c0b80df37
Maaz Nonsola <maaznonsola786@gmail.com>
2024-06-08 12:23:40 +0530

    Conditional rendering according to basket

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -16,25 +16,38 @@ function Checkout() {
           alt=""
         />
 
-        <div>
-          <h3>Hello, {user?.email}</h3>
-          <h2 className="checkout__title">Your shopping Basket</h2>
+        {basket?.length === 0 ? (
+          <div>
+            <h3>Hello, {user?.email}</h3>
+            <h2>Your shopping basket is empty</h2>
+            <p>
+              You have no items in your basket. To buy one or add item to basket
+              click the add to basket button
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h3>Hello, {user?.email}</h3>
+            <h2 className="checkout__title">Your shopping Basket</h2>
 
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+
+      {basket?.length > 0 && (
+        <div className="checkout__right">
+          <Subtotal />
         </div>
-      </div>
-
-      <div className="checkout__right">
-        <Subtotal />
-      </div>
+      )}
     </div>
   );
 }
